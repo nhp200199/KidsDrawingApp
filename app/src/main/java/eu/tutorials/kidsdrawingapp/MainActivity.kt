bde5aab2e69234f9f8051568064f2bb1c1f1d5e2
hungphuc.ng <hungphuc.ng>
2022-10-13 17:14:21 +0700

    show dialog to pick brush size

--- app/src/main/java/eu/tutorials/kidsdrawingapp/MainActivity.kt
+++ app/src/main/java/eu/tutorials/kidsdrawingapp/MainActivity.kt
@@ -1,6 +1,9 @@
 package eu.tutorials.kidsdrawingapp
 
+import android.app.Dialog
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +16,30 @@ class MainActivity : AppCompatActivity() {
 
         drawingView = findViewById(R.id.drawing_view)
         drawingView.setSizeForBrush(10f)
+
+        val ibBrushSize = findViewById<ImageButton>(R.id.ib_brush_size);
+        ibBrushSize.setOnClickListener {
+            showBrushSizeDialog()
+        }
+    }
+
+    private fun showBrushSizeDialog() {
+        val dialog = Dialog(this)
+        dialog.setContentView(R.layout.dialog_brush_size)
+        dialog.findViewById<ImageButton>(R.id.ib_small_brush).setOnClickListener {
+            drawingView.setSizeForBrush(10f)
+            dialog.dismiss()
+        }
+        dialog.findViewById<ImageButton>(R.id.ib_medium_brush).setOnClickListener {
+            drawingView.setSizeForBrush(20f)
+            dialog.dismiss()
+        }
+        dialog.findViewById<ImageButton>(R.id.ib_large_brush).setOnClickListener {
+            drawingView.setSizeForBrush(30f)
+            dialog.dismiss()
+        }
+
+        dialog.show()
     }
 
 }
